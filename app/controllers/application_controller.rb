963df6e5f9a5aa2b76993054e3b36ff8575c7ed2
samuelralak <samuelralak@ovi.com>
2014-03-27 15:59:46 +0300

    added a before_action filter for update and edit #10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :signed_in_user, only: [:edit, :update]
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   # Redirect to user profile page after successful sign in
