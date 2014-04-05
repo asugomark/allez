15c947466e47f0fee59f88f9a10975c361acd09a
asugomark <asugo27@gmail.com>
2014-04-05 18:25:06 +0300

    sorted nil object errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def show
 
-  	@user = User.find(params[:id])
-  	@avatar = @user.avatar.find(params[:id])
+  	begin
+  		@user = User.find(params[:id])
+  		@avatar = @user.avatar.find(params[:id])
+  		@avatar_count = @user.avatar.count
+  	rescue ActiveRecord::RecordNotFound
+  		logger.info "### Avatars count: #{@user.avatar.count}"
+  		@avatar_count = @user.avatar.count
+  		flash[:notice] = 'You need to update your profile picture'
+  		return   # or e.g. redirect_to :action => :index
+	end
 
   end
 end
